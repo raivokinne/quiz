82ba1dde5e6724f320e4cb2986fc5cb4c6a1ddb5
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-09-27 12:06:04 +0300

    update

--- resources/views/quiz/show.blade.php
+++ resources/views/quiz/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="container mx-auto mt-8">
+    <div class="container w-full flex justify-center items-center mt-8">
         <form action="{{ route('quiz.submit') }}" method="POST">
             @csrf
             <div class="mb-6">
@@ -10,10 +10,10 @@
                 <div class="mb-4 flex items-center gap-2">
                     <input type="radio" id="answer_{{ $answer->id }}" name="answer" value="{{ $answer->answer }}"
                         class="form-radio h-5 w-5 text-blue-600" required>
-                    <label for="answer_{{ $answer->id }}" class="ml-2 text-gray-700">{{ $answer->answer }}</label>
+                    <label for="answer_{{ $answer->id }}" class="ml-2 text-white">{{ $answer->answer }}</label>
                 </div>
             @endforeach
-            <button type="submit" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button type="submit" class="mt-4 bg-black border-2 border-white text-white font-bold py-2 px-4 rounded">
                 Submit Answer
             </button>
         </form>
